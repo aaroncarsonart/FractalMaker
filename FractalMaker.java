e68c04b5dbdd8a0760db0980e6c80c0b6ff36bae
Aaron Carson <aaroncarsonart@gmail.com>
2024-02-02 19:54:46 -0600

    Improve demo, update gitignore and README.

--- FractalMaker.java
+++ FractalMaker.java
@@ -1,333 +1,269 @@
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.Random;
 
-import javax.swing.*;
-
 /**
- * * FractalMaker is a JPanel upon which you can render images. See the
- * implementation of the method {@code render(Graphics2D g)} (which is called by
- * the FractalMaker's overridden {@code paintComponent(Graphics g)} method) to
- * change the behavior of the FractalMaker.
+ * FractalMaker is designed to render images by updating an image buffer
+ * and then painting that result onto a JPanel. Use the buttons or arrow
+ * keys to scroll through the various selected update methods for the demo.
  * <p>
- * Note: I am not an aficionado of graphics processing in Swing or in general;
- * there may be more effective ways to do this, and I am always open to
- * suggestions. The following describes my methods that seem to work teh best to
- * alter the FractalMaker's behavior and to draw things to the panel.
- * <p>
- * In the @code render(Graphics2D g)} method, you can use the BufferedImage
- * {@code image} and {@code int[] pixels} fields to easily manipulate what is
- * displayed on the FractalMaker by manipulating the pixel data of the image and
- * then drawing the resulting image on the {@code Graphics2D g} parameter.
- * Alternatively, you can also call methods directly on the the graphics context
- * such as {@code setBackground(Color)} or any other host of useful methods.
- * 
+ * All methodologies for updating the pixels array were discovered by
+ * accident. By simply playing with arithmetic, bitwise operators, and
+ * bit shifting, many unique patterns were found. A variety of different
+ * results were selected for exhibition in this demo.
+ *
  * @author Aaron Carson
  * @since Jun 23, 2014
- * @version Jan 24, 2015
+ * @version Feb 3, 2024
  */
-public class FractalMaker extends JPanel
-{
-	
-	// ********************************************************************
-	// Class constants
-	// ********************************************************************
-	
-	private static final long	serialVersionUID	= -1475062022536824052L;
-	public static final int		TILE_SIZE			= 16;
-	public static final int		SCALE				= 1;
-	public static final int		ARGB				= BufferedImage.TYPE_INT_ARGB;
-	public static final int		RGB					= BufferedImage.TYPE_INT_RGB;
-	
-	// ********************************************************************
-	// Fields
-	// ********************************************************************
-	
-	protected int				alpha;
-	protected BufferedImage		image;
-	protected int[]				pixels;
-	private Random				r;
-	
-	// ********************************************************************
-	// Constructor
-	// ********************************************************************
-	
-	/**
-	 * Create a new Render Area of the specified size.
-	 * 
-	 * @param width The width, in pixels.
-	 * @param height The height, in pixels.
-	 */
-	public FractalMaker(int width, int height) {
-		super();
-		alpha = 255;
-		setPreferredSize(new Dimension(width, height));
-		setSize(getPreferredSize());
-		setBackground(Color.BLACK);
-		r = new Random();
-		image = new BufferedImage(getWidth(), getHeight(), RGB);
-		pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
-	}
-	
-	// ********************************************************************
-	// Methods
-	// ********************************************************************
-	
-	/**
-	 * PaintComponent updates the component by drawing the data of this
-	 * RenderArea's BufferedImage directly to the component.
-	 * <p>
-	 * Note 1: overwrite paint(Graphics g) to increase performance, but no added
-	 * components are drawn.
-	 * <p>
-	 * Note 2: overwrite paintComponent(Graphics g) to draw all extra layers.
-	 * 
-	 * @see javax.swing.JComponent#paintComponent(Graphics g)
-	 */
-	// public void paint(Graphics g) {
-	// super.paint(g);
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		render((Graphics2D) g);
-	}
-	
-	/**
-	 * Assign alpha a new value. (Alpha only matters for the methods that use
-	 * alpha or if you change the image type to ARGB).
-	 * 
-	 * @param value
-	 */
-	public void setAlpha(int value) {
-		this.alpha = value;
-	}
-	
-	/**
-	 * Called by paintComponent(Graphics g) to update this FractalMaker. This
-	 * method needs to draw something to the Graphics2D parameter, or nothing
-	 * will show.
-	 * <p>
-	 * For experimentation purposes, change which methods are called in the
-	 * first section of the method to view different fractals in the GUI.
-	 * 
-	 * @param g The Graphics2D object to update.
-	 */
-	public void render(Graphics2D g) {
-		// ***********************************
-		// First, edit the image's pixel data. (try any methods below)
-		// ***********************************
-		
-		// drawRandomNoise();
-		// drawHorizontalNoiseBands();
-		// drawStripes();
-		// drawSquareFractal(2);
-		// drawBitShiftedSquareFractal(722);
-		drawBlueSquareFractal();
-		// drawGreenSquareFractal();
-		// drawRedSquareFractal();
-		// drawQuadraticFractal();
-		// drawTriangleFractal();
-		// drawFadingTriangleFractal();
-		
-		// ***************************************************
-		// Second, draw the edited image on the FractalMaker.
-		// ***************************************************
-		g.drawImage(image, null, null);
-		
-		// alternatively, use other methods like this to draw content
-		// g.setColor(Color.BLACK);
-		// g.fillRect(0, 0, getWidth(), getHeight());
-	}
-	
-	// ********************************************************************
-	// Drawing Methods
-	//
-	// Here is where the fun starts. Each method below iterates through each of
-	// pixel and modifies the value with some expression or algorithm. Some are
-	// as simple as assigning a random value, while others use some combination
-	// of arithmetic operators (+,-,*,/) along with bitwise logic operators
-	// (&,|,^,~) and bit shifting operators (<<,>>,<<<,>>>).
-	//
-	// I use those operators in conjunction with each pixel's x and y
-	// coordinates to alter the color of the images. Everything is a process of
-	// discovery for me; I am not extremely familiar with exactly what will
-	// happen, but the results can be surprising!
-	//
-	// I find the current x and y position of each pixel in the 1-dimensional
-	// integer array by doing x + y * image-width. This will successfully
-	// touch every pixel in the image when iterating with nested for-loops with
-	// a O(n) running time, where n = the number of pixels.
-	// ********************************************************************
-	
-	/**
-	 * Draws random noise on the image.
-	 */
-	public void drawRandomNoise() {
-		for (int x = 0; x < getWidth(); x++) {
-			for (int y = 0; y < getHeight(); y++) {
-				pixels[x + y * getWidth()] = r.nextInt();
-			}
-		}
-	}
-	
-	/**
-	 * Draws random noise to an integer array, having a horizontal band effect.
-	 * 
-	 * @param pixels The pixels to draw to.
-	 */
-	public void drawHorizontalNoiseBands() {
-		for (int x = 0; x < getWidth(); x++) {
-			for (int y = 0; y < getHeight(); y++) {
-				int i = x + y * getWidth();
-				pixels[i] = i & r.nextInt(256) << 8;
-			}
-		}
-	}
-	
-	/**
-	 * Draws stripes on the image.
-	 */
-	public void drawStripes() {
-		for (int x = 0; x < getWidth(); x++) {
-			for (int y = 0; y < getHeight(); y++) {
-				pixels[x + y * getWidth()] = (x * 2 + y / 2) ^ 255 << 23;
-			}
-		}
-	}
-	
-	/**
-	 * draws a square fractal to the image.
-	 */
-	public void drawSquareFractal(int divisor) {
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				int i = x + y * getWidth();
-				pixels[i] = ((x ^ y) / divisor) ^ 255 << 24;
-			}
-		}
-	}
-	
-	/**
-	 * draws a square fractal to the image.
-	 * 
-	 * @param bitShift the value to bit shift the color data by.
-	 */
-	public void drawBitShiftedSquareFractal(int bitShift) {
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				pixels[x + y * getWidth()] = ((x ^ y) / 2) << bitShift;
-			}
-		}
-	}
-	
-	/**
-	 * draws a blue square fractal to the image.
-	 */
-	public void drawBlueSquareFractal() {
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				pixels[x + y * getWidth()] = (x ^ y) / 2;
-			}
-		}
-	}
-	
-	/**
-	 * draws a green square fractal to the image.
-	 */
-	public void drawGreenSquareFractal() {
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				pixels[x + y * getWidth()] = ((x ^ y) / 2) << 8;
-			}
-		}
-	}
-	
-	/**
-	 * draws a red square fractal to the image.
-	 */
-	public void drawRedSquareFractal() {
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				pixels[x + y * getWidth()] = ((x ^ y) / 2) << 16;
-			}
-		}
-	}
-	
-	/**
-	 * Draws a quadratic fractal to the image of various colors.
-	 */
-	public void drawQuadraticFractal() {
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				pixels[x + y * getWidth()] = (~x * ~y) ^ (x ^ y) / 2;
-			}
-		}
-	}
-	
-	/**
-	 * Draw a triangle fractal to the Image.
-	 */
-	public void drawTriangleFractal() {
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				pixels[x + y * getWidth()] = (x ^ y) / ((x & y) + 1)
-						^ 255 << 24;
-			}
-		}
-	}
-	
-	/**
-	 * Draw a fading triangle fractal to the image.
-	 */
-	public void drawFadingTriangleFractal() {
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				pixels[x + y * getWidth()] = (x ^ y) / ((~x & y) + 1);
-			}
-		}
-	}
-	
-	/**
-	 * Simple description output method for when looping alpha. Used to update a
-	 * JLabel that displays teh current Alpha value.
-	 * 
-	 * @return A String.
-	 */
-	private String getAlphaDescription() {
-		return " Alpha: " + alpha;
-	}
-	
-	// *******************************************************************
-	// MAIN
-	// *******************************************************************
-	
-	/**
-	 * Test the render area in a JFrame. Run this method to view the results
-	 * using this code only.
-	 * 
-	 * @param args Does nothing.
-	 */
-	public static void main(String[] args) {
-		final FractalMaker p = new FractalMaker(512, 512);
-		final JLabel label = new JLabel(p.getAlphaDescription());
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFrame frame = new JFrame("RenderArea Test");
-				frame.getContentPane().add(p, BorderLayout.CENTER);
-				frame.getContentPane().add(label, BorderLayout.SOUTH);
-				frame.pack();
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setLocationRelativeTo(null);
-				frame.setVisible(true);
-			}
-		});
-	}
+public class FractalMaker extends JPanel {
+    public static final int ALGORITHM_COUNT = 15;
 
-    /**
-     * This is a test method that was written with vim.
-     */
-    public static void testMethod(){
-        System.out.println("I wrote this method with vim.");
+    private final BufferedImage image;
+    private final int[] pixels;
+    private final Random random;
+    private int index;
+
+    public FractalMaker(int width, int height) {
+        super();
+        setPreferredSize(new Dimension(width, height));
+        setSize(getPreferredSize());
+        setBackground(Color.BLACK);
+        random = new Random();
+        image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+        index = 0;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        render((Graphics2D) g);
+    }
+
+    public void render(Graphics2D g) {
+        g.drawImage(image, null, null);
+    }
+
+    public void drawSquareFractal(int divisor, int bitShift) {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                pixels[x + y * getWidth()] = ((x ^ y) / divisor) << bitShift;
+            }
+        }
+    }
+
+    public void drawBlueSquareFractal() {
+        drawSquareFractal(2, 0);
+    }
+
+    public void drawGreenSquareFractal() {
+        drawSquareFractal(2, 8);
+    }
+
+    public void drawRedSquareFractal() {
+        drawSquareFractal(2, 16);
+    }
+
+    public void drawRandomNoise() {
+        for (int x = 0; x < getWidth(); x++) {
+            for (int y = 0; y < getHeight(); y++) {
+                pixels[x + y * getWidth()] = random.nextInt();
+            }
+        }
+    }
+
+    public void drawHorizontalNoiseBands() {
+        for (int x = 0; x < getWidth(); x++) {
+            for (int y = 0; y < getHeight(); y++) {
+                int i = x + y * getWidth();
+                pixels[i] = i & random.nextInt(256) << 8;
+            }
+        }
+    }
+
+    public void drawVerticalAngledStripes() {
+        for (int x = 0; x < getWidth(); x++) {
+            for (int y = 0; y < getHeight(); y++) {
+                pixels[x + y * getWidth()] = (x * 2 + y / 2) ^ 255 << 23;
+            }
+        }
+    }
+
+    public void drawQuadraticFractal() {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                pixels[x + y * getWidth()] = (~x * ~y) ^ (x ^ y) / 2;
+            }
+        }
+    }
+
+    public void drawTriangleFractal() {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                pixels[x + y * getWidth()] = (x ^ y) / ((x & y) + 1) ^ 255 << 24;
+            }
+        }
+    }
+
+    public void drawFadingTriangleFractal() {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                pixels[x + y * getWidth()] = (x ^ y) / ((~x & y) + 1);
+            }
+        }
+    }
+
+    private void updateFractalImage() {
+        switch (index) {
+            // Bit shifting to change the effected color channel.
+            case 0:
+                drawBlueSquareFractal();
+                break;
+            case 1:
+                drawGreenSquareFractal();
+                break;
+            case 2:
+                drawRedSquareFractal();
+                break;
+            // Scaling the size.
+            case 3:
+                drawSquareFractal(1, 0);
+                break;
+            case 4:
+                drawSquareFractal(2, 18);
+                break;
+            case 5:
+                drawSquareFractal(2, 12);
+                break;
+            // Odd divisors change the pattern.
+            // Increasing the bit shift between RGB channel boundaries zooms out.
+            case 6:
+                drawSquareFractal(7, 5);
+                break;
+            case 7:
+                drawSquareFractal(3, 21);
+                break;
+            case 8:
+                drawSquareFractal(5, 13);
+                break;
+            // Other interesting patterns.
+            case 9:
+                drawRandomNoise();
+                break;
+            case 10:
+                drawHorizontalNoiseBands();
+                break;
+            case 11:
+                drawVerticalAngledStripes();
+                break;
+            case 12:
+                drawQuadraticFractal();
+                break;
+            // The Sierpiński triangle.
+            case 13:
+                drawTriangleFractal();
+                break;
+            case 14:
+                drawFadingTriangleFractal();
+                break;
+        }
+    }
+
+    private void onLeftButtonClick() {
+        index--;
+        if (index < 0) {
+            index = ALGORITHM_COUNT - 1;
+        }
+        updateFractalImage();
+        repaint();
+    }
+
+    private void onRightButtonClick() {
+        index++;
+        if (index >= ALGORITHM_COUNT) {
+            index = 0;
+        }
+        updateFractalImage();
+        repaint();
+    }
+
+    private static class LeftRightKeyListener implements KeyListener {
+        private final Runnable onLeft;
+        private final Runnable onRight;
+
+        public LeftRightKeyListener(Runnable onLeft, Runnable onRight) {
+            this.onLeft = onLeft;
+            this.onRight = onRight;
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {}
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                onLeft.run();
+            } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                onRight.run();
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {}
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            FractalMaker fractalMaker = new FractalMaker(512, 512);
+            fractalMaker.updateFractalImage();
+
+            JFrame frame = new JFrame("Fractal Maker");
+            Container contentPane = frame.getContentPane();
+            contentPane.add(fractalMaker, BorderLayout.CENTER);
+
+            JPanel buttonPane = new JPanel(new FlowLayout());
+            JButton leftButton = new JButton("<");
+            JButton rightButton = new JButton(">");
+            leftButton.addActionListener(event -> fractalMaker.onLeftButtonClick());
+            rightButton.addActionListener(event -> fractalMaker.onRightButtonClick());
+            buttonPane.add(leftButton);
+            buttonPane.add(rightButton);
+            contentPane.add(buttonPane, BorderLayout.SOUTH);
+
+            Runnable onLeft = () -> {
+                fractalMaker.onLeftButtonClick();
+                leftButton.requestFocusInWindow();
+            };
+            Runnable onRight = () -> {
+                fractalMaker.onRightButtonClick();
+                rightButton.requestFocusInWindow();
+            };
+            KeyListener keyListener = new LeftRightKeyListener(onLeft, onRight);
+
+            fractalMaker.addKeyListener(keyListener);
+            frame.addKeyListener(keyListener);
+            leftButton.addKeyListener(keyListener);
+            rightButton.addKeyListener(keyListener);
+
+            frame.pack();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            frame.requestFocus();
+        });
     }
 }
--- README.md
+++ README.md
@@ -1,58 +1,20 @@
 # FractalMaker
-#### An Java project I created by accident that happens to be really cool, so I'm making it open source.
+FractalMaker is a simple Swing application that demonstrates various
+ways for how to modify a pixel buffer with arithmetic, biwise
+operations, and bitshifts in order to generate a series of complex
+and interesting patterns in the resulting image.
 
-***Author:***   Aaron Carson  
-***Language:*** Java  
+All methodologies presented in this demo were discovered by accident.
 
-## Quick Start  
-From the command line, just download the code, compile and run:  
+![Alt text](fractal.png)
 
-	javac FractalMaker.java
-	java FractalMaker
+## Build and Run
+The following builds and runs the single-file program.
 
-to make this:
-
-![Alt text](fractal.png) 
-
-##Description
-Do cool things with fractals!  Play with pixel data with a single Java source file.  It's really easy: the pixel data is stored as a 1-dimensional integer array.  So, most of the pixel data is edited like this:
-
-```java 
-for (int x = 0; x < image.getWidth(); x++) {
-	for (int y = 0; y < image.getHeight(); y++) {
-		pixels[x + y * getWidth()] = (x ^ y) / 2;
-	}
-}
+```zsh
+javac FractalMaker.java
+java FractalMaker
 ```
 
-This this double for-loop walks through each pixel, and draws the image above.  I have written a number of methods which make cool images like this, that can be called in the method `render(Graphics2D g)`.  Try modifying the code by commenting different methods out to get fun results! (only the last called method will run):
-
-```java
-public void render(Graphics2D g) {
-	// ***********************************
-	// First, edit the image's pixel data. (try any methods below)
-	// ***********************************
-	
-	// drawRandomNoise();
-	// drawHorizontalNoiseBands();
-	// drawStripes();
-	// drawSquareFractal(2);
-	// drawBitShiftedSquareFractal(722);
-	drawBlueSquareFractal();	       // comment this out and try something else!
-	// drawGreenSquareFractal();
-	// drawRedSquareFractal();
-	// drawQuadraticFractal();
-	// drawTriangleFractal();
-	// drawFadingTriangleFractal();
-		
-	// ***************************************************
-	// Second, draw the edited image on the FractalMaker.
-	// ***************************************************
-	g.drawImage(image, null, null);
-		
-	// alternatively, use other methods like this to draw content
-	// g.setColor(Color.BLACK);
-	// g.fillRect(0, 0, getWidth(), getHeight());
-}
-```
-Have fun with my code.  Feel free to share with me what you know, or can do with this.
+Use the buttons or arrow keys to cycle through a series of curated
+examples of different ways to alter the pixel data.
